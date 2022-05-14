54d7579fe4b8d9ac1c93afcebd34c8f247a30e78
moe <moe.aoki0124@gmail.com>
2022-05-14 11:25:19 +1000

    started  javascript to add new items to the list

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function addToList(){
+    let newItem = document.getElementById("new-item").value;
+    document.getElementById("list-item").innerHTML = newItem;
+}
+
+// function appendToList(){
+//     let li = document.createElement("li");
+//     let inputValue = 
+// }
