5aa04d37b19091fb201e167fd5a42165a95fb5c7
moe <moe.aoki0124@gmail.com>
2022-05-14 22:26:28 +1000

    refactoring js

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
 
-function addToList(){
-    let newItem = document.getElementById("new-item").value;
-    document.getElementById("list-item").innerHTML = newItem;
-}
+
+
+
+
+
+
+
+
+
+
+// let addButton = document.getElementById("add-button");
+// let listItems = document.getElementById("list-items");
+// let newItem = document.getElementById("new-item");
+
+// addButton.addEventListener("click", function(){
+//     let list = document.createElement("li");
+//     list.innerText = newItem.value;
+//     listItems.appendChild(list);
+// })
+
+
+
+
+
+
+
+
+
+
+// let newItem = document.getElementById("new-item");
+
+
+// function addToList(){
+//     newItem = document.getElementById("new-item").value;
+//     document.getElementById("list-items").innerHTML = newItem;
+// }
 
 // function appendToList(){
 //     let li = document.createElement("li");
-//     let inputValue = 
+//     li.innerText = newItem
 // }
